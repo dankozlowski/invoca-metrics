63f8a22315e6ab937cef4de269cca2470a060854
Colin Kelley <colindkelley@gmail.com>
2015-05-07 11:29:02 -0700

    Slight reformat. Use class << self so private works

--- lib/invoca/metrics/direct_metric.rb
+++ lib/invoca/metrics/direct_metric.rb
@@ -15,58 +15,63 @@ module Invoca
         "#{name} #{value} #{tick}"
       end
 
-      def self.report(metrics)
-        send_to_sensu(
-            :name => "application_metrics",
-            :type => "metric",
-            :command => "rr_metrics",
-            :output => [metrics].flatten.map { |m| m.to_s }.join("\n") + "\n",
-            :handlers => ["graphite"],
-            :issued => Time.now.to_i,
-            :executed => Time.now.to_i,
-            :standalone => true,
-            :interval => 60,
-            :subscribers => ["all"]
-        )
-      end
+      PERIOD = 60
 
-      def self.generate_distribution(metric_prefix,metric_values,tick = nil)
-        fixed_tick = tick || rounded_tick
-        sorted_values = metric_values.sort
-        count = sorted_values.count
+      class << self
+        def report(metrics)
+          timeslot = Time.now.to_i
+          metrics_output = [metrics].flatten.map { |m| m.to_s }.join("\n") + "\n"
 
-        metrics =
-            if count == 0
-              [
+          send_to_sensu(
+              name:         "application_metrics",
+              type:         "metric",
+              command:      "rr_metrics",
+              output:       metrics_output,
+              handlers:     ["graphite"],
+              issued:       timeslot,
+              executed:     timeslot,
+              standalone:   true,
+              interval:     PERIOD,
+              subscribers:  ["all"]
+          )
+        end
+
+        def generate_distribution(metric_prefix, metric_values, tick = nil)
+          fixed_tick = tick || rounded_tick
+          sorted_values = metric_values.sort
+          count = sorted_values.count
+
+          metrics =
+              if count == 0
+                [
                   new("#{metric_prefix}.count",    count,                    fixed_tick)
-              ]
-            else
-              [
+                ]
+              else
+                [
                   new("#{metric_prefix}.count",    count,                    fixed_tick),
                   new("#{metric_prefix}.max",      sorted_values[-1],        fixed_tick),
                   new("#{metric_prefix}.min",      sorted_values[0],         fixed_tick),
                   new("#{metric_prefix}.median",   sorted_values[count*0.5], fixed_tick),
                   new("#{metric_prefix}.upper_90", sorted_values[count*0.9], fixed_tick)
-              ]
-            end
+                ]
+              end
+        end
+
+        private
+
+        SENSU_PORT = 3030
+        SENSU_HOST = '127.0.0.1'
+
+        def send_to_sensu(message)
+          UDPSocket.new.send(message.to_json + "\n", 0, SENSU_HOST, SENSU_PORT)
+        end
+
+        def rounded_tick
+          tick = Time.now.to_i
+          tick - (tick % PERIOD)
+        end
+
       end
-
-      private
-
-      SENSU_PORT = 3030
-      SENSU_HOST = '127.0.0.1'
-
-      def self.send_to_sensu(message)
-        UDPSocket.new.send(message.to_json + "\n", 0, SENSU_HOST, SENSU_PORT)
-      end
-
-      def self.rounded_tick
-        tick = Time.now.to_i
-        tick - (tick % 60)
-      end
-
     end
   end
 end
-
-
