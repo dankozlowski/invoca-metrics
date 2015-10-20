0dcb1bf7066ede3dd5ebd98ca7e10d612b3ae049
melch <melch@invoca.com>
2015-10-19 23:34:28 -0700

    Use present? instead of presence when expecting boolean only.

--- lib/invoca/metrics/client.rb
+++ lib/invoca/metrics/client.rb
@@ -53,7 +53,7 @@ module Invoca
       end
 
       def timer(name, milliseconds = nil, &block)
-        name.presence or raise ArgumentError, "Must specify a metric name."
+        name.present? or raise ArgumentError, "Must specify a metric name."
         (!milliseconds.nil? ^ block_given?) or raise ArgumentError, "Must pass exactly one of milliseconds or block."
         name_and_type = [name, "timer", @server_name].join(STATSD_METRICS_SEPARATOR)
 
@@ -84,7 +84,7 @@ module Invoca
     protected
 
       def metric_args(name, value, stat_type)
-        name.presence or raise ArgumentError, "Must specify a metric name."
+        name.present? or raise ArgumentError, "Must specify a metric name."
         extended_name = [name, stat_type, @server_name, @sub_server_name].compact.join(STATSD_METRICS_SEPARATOR)
         if value
           [extended_name, value]
